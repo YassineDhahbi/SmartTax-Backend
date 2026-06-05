cb1f91f9f82e5438e2eb93a0591e4a71c5647017
Yasssa02 <yassinedhahbi65@gmail.com>
2026-06-05 13:42:17 +0100

    ci: add Maven pipeline /  SonarCloud for backend

--- src/test/java/tn/esprit/arabsoftback/ArabSoftBackApplicationTests.java
+++ src/test/java/tn/esprit/arabsoftback/ArabSoftBackApplicationTests.java
@@ -2,9 +2,15 @@ package tn.esprit.arabsoftback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import tn.esprit.arabsoftback.notification.NotificationEvent;
 
 @SpringBootTest
 class ArabSoftBackApplicationTests {
+
+    @MockBean
+    private KafkaTemplate<String, NotificationEvent> kafkaTemplate;
 
     @Test
     void contextLoads() {
